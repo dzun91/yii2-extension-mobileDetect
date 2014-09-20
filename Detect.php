b0f91325d4be30d51efa08a815f2318e2e5ef0b4
dzun91 <dzungnv91@gmail.com>
2014-09-20 17:11:37 +0700

    Commit first source

---
+++ Detect.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace extensions\mobiledetect;
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'Mobile_Detect.php';
+
+class Detect extends \yii\base\Component
+{
+    protected $_mobiledetect;
+
+    public function init(){
+        parent::init();
+        $this->_mobiledetect = new \Mobile_Detect();
+    }
+
+    public function __call($name,$arguments){
+        return call_user_func_array(array($this->_mobiledetect, $name), $arguments);
+    }
+}
